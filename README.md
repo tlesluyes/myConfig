1ef9ac85251cf20cc6807b756a740795e40ef471
Tom Lesluyes <t.lesluyes@gmail.com>
2024-02-16 11:02:23 +0100

    Add bashrc.sh

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # myConfig
 My useful configuration files.
+
+## Bash
+
+### [`bashrc.sh`](bash/bashrc.sh)
+
+It modifies the PS1 variable, sets aliases, configures the auto-completion and defines the following functions:
+  - `files $1`: count the number of files/directories/links in the current folder (no $1) or in a given folder ($1).
+  - `up $1`: jump to X many parent directories (1 without $1).
+  - `extract $1`: extract a given archive ($1) with the right tool and/or options.
+
+**Installation**: add `source $somePath/myConfig/bash/bashrc.sh` to your `~/.bashrc`.
---
+++ bash/bashrc.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+# This function sets the PS1 variable
+function prompt_command {
+    local EXIT_CODE=$?
+    # Set background and foreground colors
+    local RESET="\[\033[0m\]" #0m restores to the terminal's default colour
+    local DIVIDER="" # U+E0B0
+    local DIVIDER2=""
+    local WHITE="\[\033[97m\]"
+    local WHITE_BOLD="\[\033[1;37m\]"
+    local BG_DEFAULT="\[\033[49m\]"
+    local BG_TIME="\[\033[48;2;105;121;16m\]"
+    local FG_TIME="\[\033[38;2;105;121;16m\]"
+    local BG_USER="\[\033[48;2;221;57;57m\]"
+    local FG_USER="\[\033[38;2;221;57;57m\]"
+    local FG_PATH="\[\033[38;2;0;0;223m\]"
+    local BG_PATH="\[\033[48;2;0;0;223m\]"
+    local FG_GIT="\[\033[38;2;0;139;139m\]"
+    local BG_GIT="\[\033[48;2;0;139;139m\]"
+    local FG_CONDA="\[\033[38;5;172m\]"
+    local BG_CONDA="\[\033[48;5;172m\]"
+    local FG_ERROR="\[\033[38;2;255;0;0m\]"
+    local BG_ERROR="\[\033[48;2;255;0;0m\]"
+    # Set the user name
+    local myUSER="TBD"
+    case $HOSTNAME-$USER in 
+        Kitsune-*)                      myUSER="Kitsune" ;;
+        zen-*)                          myUSER="Zen" ;;
+        madamete-*)                     myUSER="Madamete" ;;
+        hpclogin-*)                     myUSER="HPC" ;;
+        C02YP5FZLVDC-*)                 myUSER="Tom" ;;
+        login*.nemo.thecrick.org-*)     myUSER="Nemo" ;;
+    esac
+    # Check the exit code of the last executed command
+    local ERRMSG=""
+    if [ $EXIT_CODE != 0 ]; then
+        ERRMSG="[exit=$EXIT_CODE]"
+    fi
+    # Check if the folder is a git repo
+    local BRANCH=""
+    local GIT_LOCAL_CHANGES=""
+    if which git &>/dev/null; then
+        local REMOTE=""
+        local STATE=""
+        local branch_pattern="^On branch ([a-zA-Z0-9_\-]*)" # This captures the branch name
+        local remote_pattern="Your branch is (ahead|behind|up to date) " # This captures the status
+        local diverge_pattern="Your branch and (.*) have diverged" # This captures the status
+        local git_status="$(LC_ALL=C git status 2> /dev/null)"
+        if [ "$git_status" != "" ] ; then
+            # This sets a change
+            if [[ ! ${git_status} =~ "working tree clean" ]]; then
+                STATE="±"
+            fi
+            # If the pattern is captured, set A (ahead), B (behind) or empty (up-to-date)
+            if [[ ${git_status} =~ ${remote_pattern} ]]; then
+                if [[ ${BASH_REMATCH[1]} == "ahead" ]]; then
+                    REMOTE="A"
+                elif [[ ${BASH_REMATCH[1]} == "behind" ]]; then
+                    REMOTE="B"
+                else
+                    REMOTE=""
+                fi
+            # If not ahead, behind or up-to-date, set U (unkown)
+            else
+                REMOTE="U"
+            fi
+            # This sets divergent
+            if [[ ${git_status} =~ ${diverge_pattern} ]]; then
+                REMOTE="${REMOTE}D"
+            fi
+            # Get the branch name
+            if [[ ${git_status} =~ ${branch_pattern} ]]; then
+                BRANCH=${BASH_REMATCH[1]}
+            fi
+            # Combine information
+            GIT_LOCAL_CHANGES="${REMOTE}${STATE}"
+            # Reformat if there is something to display
+            if [ "$GIT_LOCAL_CHANGES" != "" ]; then
+                GIT_LOCAL_CHANGES=" (${GIT_LOCAL_CHANGES})"
+            fi
+        fi
+    fi
+    # Check if conda is set (only show non-"base" environments)
+    local CONDA=$CONDA_DEFAULT_ENV
+    if [ "$CONDA" == "base" ]; then CONDA=""; fi
+    # Create the PS1 variable
+    local QUERY=""
+    QUERY+="${BG_TIME}${WHITE}[\t]${FG_TIME}"
+    QUERY+="${BG_USER}${DIVIDER}${WHITE}${myUSER}${FG_USER}"
+    QUERY+="${BG_PATH}${DIVIDER}${WHITE}\w${RESET}${FG_PATH}"
+    if [ "$CONDA" != "" ]; then QUERY+="${BG_CONDA}${DIVIDER}${WHITE}${CONDA}${FG_CONDA}"; fi
+    if [ "$BRANCH" != "" ]; then QUERY+="${BG_GIT}${DIVIDER}${WHITE}${BRANCH}${GIT_LOCAL_CHANGES}${FG_GIT}"; fi
+    if [ "$ERRMSG" != "" ]; then QUERY+="${BG_ERROR}${DIVIDER}${WHITE_BOLD}${ERRMSG}${FG_ERROR}"; fi
+    QUERY+="${BG_DEFAULT}${DIVIDER}${RESET} "
+    export PS1=$QUERY
+}
+
+# This function counts the number of files/directories/links in the current folder (no $1) or in a given folder ($1)
+function files {
+    local FILES="\033[31m" # Red
+    local DIRECTORIES="\033[34m" # Blue
+    local LINKS="\033[36m" # Cyan
+    local RESET="\033[0m"
+    if [ "$1" != "" ]; then
+        if [ -d "$1" ]; then
+            LOC="$1"
+        else
+            echo "\$1 ($1) is not a valid directory"
+            return 1
+        fi
+    else
+        LOC="."
+    fi
+    local nFILES=$(find $LOC -maxdepth 1 -type f | wc -l | sed -r "s/^ +//g")
+    local nDIRECTORIES=$(find $LOC -mindepth 1 -maxdepth 1 -type d | wc -l | sed -r "s/^ +//g")
+    local nLINKS=$(find $LOC -maxdepth 1 -type l | wc -l | sed -r "s/^ +//g")
+    echo -e "Under \"$LOC\": ${FILES}${nFILES} files${RESET}; ${DIRECTORIES}${nDIRECTORIES} directories${RESET}; ${LINKS}${nLINKS} links${RESET}"
+}
+
+# This function jumps to X many parent directories (1 without $1)
+function up {
+  local d=""
+  local limit=$1
+  for ((i=1 ; i <= limit ; i++)); do
+      d=$d/..
+  done
+  d=$(echo $d | sed 's/^\///')
+  if [ -z "$d" ]; then
+    d=..
+  fi
+  cd $d
+}
+
+# This function extracts a given archive ($1) with the right tool and/or options
+function extract {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1   ;;
+      *.tar.gz)    tar xzf $1   ;;
+      *.bz2)       bunzip2 $1   ;;
+      *.rar)       unrar x $1   ;;
+      *.gz)        gunzip $1    ;;
+      *.tar)       tar xf $1    ;;
+      *.tbz2)      tar xjf $1   ;;
+      *.tgz)       tar xzf $1   ;;
+      *.zip)       unzip $1     ;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1      ;;
+      *)           echo "'$1' cannot be extracted via extract()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file!"
+  fi
+}
+
+# Ignore case when using tab to autocomplete path
+bind 'set completion-ignore-case on'
+# Show auto-completion list automatically, without double tab
+bind "set show-all-if-ambiguous on"
+# give same rights to the group and user to everything I create
+umask 0002
+# Allow ctrl-S for history navigation (with ctrl-R)
+stty -ixon
+# Expand the history size
+export HISTFILESIZE=10000
+export HISTSIZE=500
+# Don't put duplicate lines in the history and do not add lines that start with a space
+export HISTCONTROL=erasedups:ignoreboth
+# Append to the history file, don't overwrite it
+shopt -s histappend
+# Check the window size after each command and, if necessary, update the values of LINES and COLUMNS
+shopt -s checkwinsize
+# My alliases
+alias grep='grep --color=auto'
+alias zgrep='zgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ls='ls --color=auto'
+alias lsl='ls -lsah'
+alias cd.='cd ../'
+alias less='less -S'
+alias vi='vim'
+alias squ="squeue -u $USER"
+alias sacctm='sacct --units=G --format JobID,JobName%60,Partition,Account,User,AllocCPUS,Elapsed,CPUTime,MaxRSS,NodeList,State,ExitCode'
+# Set PROMPT_COMMAND
+export PROMPT_COMMAND=prompt_command
